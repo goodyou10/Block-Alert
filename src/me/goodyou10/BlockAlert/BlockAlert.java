b69ce19c4037855c2816efc1fbd109e247bce93f
Ashley Lavery <paxton26@hotmail.co.uk>
2012-05-08 18:04:59 +0100

    Updated for version 0.1

---
+++ src/me/goodyou10/BlockAlert/BlockAlert.java
@@ -0,0 +1,52 @@
+package me.goodyou10.BlockAlert;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import me.goodyou10.BlockAlert.Listeners.BreakListener;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BlockAlert extends JavaPlugin {
+	
+	Logger log = Logger.getLogger("Minecraft");
+	
+	public boolean debug = false;
+	public static double version = 0.1;
+	
+	public void onEnable() {
+		new BreakListener(this);
+		
+		log("Plugin loaded!");
+	}
+	
+	public void onDisable() {
+		log("Plugin unloaded!");
+	}
+	
+	public void msgAdmins(String message) {
+		for (Player player : this.getServer().getOnlinePlayers()) {
+			if(player.hasPermission("blockalert.admin")) {
+				player.sendMessage(ChatColor.GREEN + "[BlockAlert] " + message);
+			}
+		}
+	}
+	
+	public void log(String message) {
+		log.info("[BlockAlert " + version + "]: " + message);
+	}
+	
+	public void debug(String message) {
+		if(debug) {
+			log.log(Level.WARNING, "[BlockAlert Debug]: " + message);
+		}
+	}
+	
+	public void debug(Exception e) {
+		if(debug) {
+			log.log(Level.SEVERE, "[BlockAlert Exception]: " + e.toString());
+		}
+	}
+}
---
+++ src/me/goodyou10/BlockAlert/Listeners/BreakListener.java
@@ -0,0 +1,30 @@
+package me.goodyou10.BlockAlert.Listeners;
+
+import me.goodyou10.BlockAlert.BlockAlert;
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class BreakListener implements Listener {
+	
+	private BlockAlert plugin;
+	
+	public BreakListener(BlockAlert plugin) {
+		this.plugin = plugin;
+		this.plugin.getServer().getPluginManager().registerEvents(this, this.plugin);
+	}
+	
+	@EventHandler(ignoreCancelled = true)
+	public void onBlockBreak(BlockBreakEvent event) {
+		Player player = event.getPlayer();
+		String name = player.getName();
+		Block block = event.getBlock();
+		String bName = block.getType().name().toLowerCase();
+		
+		this.plugin.log(name + " has broken " + bName);
+		this.plugin.msgAdmins(name + " has broken " + bName);
+	}
+}
